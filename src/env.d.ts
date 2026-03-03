e9c019ec2f2eef5b64a3468bf31c2b6d4bd56c39
ccwq <scwqd@qq.com>
2026-03-03 09:30:49 +0800

    Updates

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare const __APP_VERSION__: string
-
-declare module '~icons/*' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
